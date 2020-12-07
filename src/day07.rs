f606bacb0c019b6d67f36a569fd8197d319fd08e
Jacob Pedersen <jacobsqy@teknik.io>
2020-12-07 22:05:50 +0100

    Day7: Optimize part 1 and parsing

--- src/day07.rs
+++ src/day07.rs
@@ -7,70 +7,77 @@ use std::collections::HashSet;
 
 pub fn run(puzzle: &Puzzle) -> Result<RunResult, RunError> {
     match puzzle.part {
-        Part::One => Ok(RunResult::U32(part1(&parse(&puzzle.input)))),
-        Part::Two => Ok(RunResult::U32(part2(&parse(&puzzle.input)))),
-        //_ => Err(RunError::NoResult),
+        Part::One => Ok(RunResult::U32(part1(&parse1(&puzzle.input)))),
+        Part::Two => Ok(RunResult::U32(part2(&parse2(&puzzle.input)))),
     }
 }
 
-fn parse(input: &str) -> HashMap<&str, Vec<(u32, &str)>> {
-    let mut result: HashMap<&str, Vec<(u32, &str)>> = HashMap::new();
+fn parse1(input: &str) -> HashMap<&str, Vec<&str>> {
+    let mut result: HashMap<&str, Vec<&str>> = HashMap::new();
     for line in input.lines() {
+        let mut split = line.split(" contain ");
+        let parent = split.next().unwrap().rsplit_once(' ').unwrap().0;
+        let children = split.next().unwrap().split(", ");
+        for child in children {
+            let child = child.split_once(' ').unwrap();
+            result
+                .entry(child.1.rsplit_once(' ').unwrap().0)
+                .and_modify(|v| v.push(parent))
+                .or_insert(vec![parent]);
+        }
+    }
+    result
+}
+
+fn parse2(input: &str) -> HashMap<&str, Vec<(&str, &str)>> {
+    let mut result: HashMap<&str, Vec<(&str, &str)>> = HashMap::new();
+    for line in input.lines() {
+        let mut num_bags = Vec::new();
+
         let mut split = line.split(" contain ");
         let bag = split.next().unwrap().rsplit_once(' ').unwrap().0;
         let content = split.next().unwrap().split(", ");
         for num_bag in content {
-            if num_bag != "no other bags." {
-                let num_bag = num_bag.split_once(' ').unwrap();
-                let num_bag = (
-                    num_bag.0.parse::<u32>().unwrap(),
-                    num_bag.1.rsplit_once(' ').unwrap().0,
-                );
-                result
-                    .entry(bag)
-                    .and_modify(|v| v.push(num_bag))
-                    .or_insert(vec![num_bag]);
-            }
+            let num_bag = num_bag.split_once(' ').unwrap();
+            let num_bag = (num_bag.0, num_bag.1.rsplit_once(' ').unwrap().0);
+            num_bags.push(num_bag);
         }
+        result.insert(bag, num_bags);
     }
     result
 }
 
-fn part1(rules: &HashMap<&str, Vec<(u32, &str)>>) -> u32 {
+fn part1(rules: &HashMap<&str, Vec<&str>>) -> u32 {
     part1_helper(rules, "shiny gold").len() as u32 - 1
 }
 
-fn part1_helper(rules: &HashMap<&str, Vec<(u32, &str)>>, bag_to_find: &str) -> HashSet<String> {
+fn part1_helper(rules: &HashMap<&str, Vec<&str>>, parent: &str) -> HashSet<String> {
     let mut ancestors = HashSet::new();
-    ancestors.insert(String::from(bag_to_find));
-    for parent_bag in find_parent_bags(rules, bag_to_find) {
-        for ancestor in part1_helper(rules, &parent_bag) {
-            ancestors.insert(ancestor);
+    ancestors.insert(String::from(parent));
+    match rules.get(parent) {
+        Some(parents) => {
+            for parent in parents {
+                for ancestor in part1_helper(rules, parent) {
+                    ancestors.insert(ancestor);
+                }
+            }
         }
+        None => (),
     }
     ancestors
 }
 
-fn find_parent_bags(rules: &HashMap<&str, Vec<(u32, &str)>>, bag_to_find: &str) -> HashSet<String> {
-    let mut result = HashSet::new();
-    for (parent_bag, content) in rules {
-        for (_, bag) in content {
-            if *bag == bag_to_find {
-                result.insert(String::from(*parent_bag));
-            }
-        }
-    }
-    result
-}
-
-fn part2(rules: &HashMap<&str, Vec<(u32, &str)>>) -> u32 {
+fn part2(rules: &HashMap<&str, Vec<(&str, &str)>>) -> u32 {
     part2_helper(rules, "shiny gold").unwrap_or(1) - 1
 }
 
-fn part2_helper(rules: &HashMap<&str, Vec<(u32, &str)>>, parent: &str) -> Option<u32> {
+fn part2_helper(rules: &HashMap<&str, Vec<(&str, &str)>>, parent: &str) -> Option<u32> {
     let mut result: u32 = 1;
     for child in rules.get(parent)? {
-        result += child.0 * part2_helper(rules, child.1).unwrap_or(1);
+        match child.0.parse::<u32>() {
+            Ok(n) => result += n * part2_helper(rules, child.1).unwrap_or(1),
+            _ => (),
+        }
     }
     Some(result)
 }
@@ -81,22 +88,28 @@ mod tests {
     use test::Bencher;
 
     #[bench]
-    fn bench_parse(b: &mut Bencher) {
+    fn bench_parse1(b: &mut Bencher) {
         let puzzle: Puzzle = Puzzle::new("7", "1", None).unwrap();
-        b.iter(|| parse(&puzzle.input));
+        b.iter(|| parse1(&puzzle.input));
+    }
+
+    #[bench]
+    fn bench_parse2(b: &mut Bencher) {
+        let puzzle: Puzzle = Puzzle::new("7", "2", None).unwrap();
+        b.iter(|| parse2(&puzzle.input));
     }
 
     #[bench]
     fn bench_part1(b: &mut Bencher) {
         let puzzle: Puzzle = Puzzle::new("7", "1", None).unwrap();
-        let parsed_input = parse(&puzzle.input);
+        let parsed_input = parse1(&puzzle.input);
         b.iter(|| part1(&parsed_input));
     }
 
     #[bench]
     fn bench_part2(b: &mut Bencher) {
         let puzzle: Puzzle = Puzzle::new("7", "2", None).unwrap();
-        let parsed_input = parse(&puzzle.input);
+        let parsed_input = parse2(&puzzle.input);
         b.iter(|| part2(&parsed_input));
     }
 }
