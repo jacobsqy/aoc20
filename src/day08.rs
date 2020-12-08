b71b755d6dc80593ebeb4ea0abaed6d7831165a3
Jacob Pedersen <jacobsqy@teknik.io>
2020-12-08 10:05:06 +0100

    Day8: Implement part 2 in a functional style
    Also change type conversions in part 1 to speed it up.

--- src/day08.rs
+++ src/day08.rs
@@ -8,28 +8,26 @@ pub fn run(puzzle: &Puzzle) -> Result<RunResult, RunError> {
     match puzzle.part {
         Part::One => part1(&puzzle.input.lines().collect()),
         Part::Two => part2(&puzzle.input.lines().collect()),
-        _ => Err(RunError::NoResult),
     }
 }
 
 fn part1(lines: &Vec<&str>) -> Result<RunResult, RunError> {
-    let mut visited_values: HashSet<usize> = HashSet::new();
+    let mut visited_values: HashSet<i32> = HashSet::new();
     let mut accumulator: i32 = 0;
-    let mut i = 0;
-    while i < lines.len() {
+    let mut i: i32 = 0;
+    let last_instruction = lines.len() as i32;
+    while i != last_instruction {
         if !visited_values.insert(i) {
             // Return the accumulator if the value was already in the HashSet
             return Err(RunError::NoResult);
         }
 
-        match lines[i].split_once(' ') {
+        match lines[i as usize].split_once(' ') {
             Some(("acc", x)) => {
                 accumulator += x.parse::<i32>().unwrap();
                 i += 1;
             }
-            Some(("jmp", x)) => {
-                i = ((i as i32) + x.parse::<i32>().unwrap()) as usize;
-            }
+            Some(("jmp", x)) => i += x.parse::<i32>().unwrap(),
             Some(("nop", _)) => i += 1,
             _ => (),
         }
@@ -39,22 +37,20 @@ fn part1(lines: &Vec<&str>) -> Result<RunResult, RunError> {
 }
 
 fn part2(lines: &Vec<&str>) -> Result<RunResult, RunError> {
-    for i in 0..lines.len() {
-        if lines[i].starts_with("jmp") {
-            let new_line = lines[i].replace("jmp", "nop");
-            match part1(&[&lines[0..i], &[&new_line], &lines[i + 1..]].concat()) {
-                Err(_) => (),
-                r => return r,
+    lines
+        .iter()
+        .enumerate()
+        .filter(|(_i, l)| l.starts_with("jmp") || l.starts_with("nop"))
+        .map(|(i, l)| {
+            if l.starts_with("jmp") {
+                part1(&[&lines[0..i], &[&l.replace("jmp", "nop")], &lines[i + 1..]].concat())
+            } else {
+                part1(&[&lines[0..i], &[&l.replace("nop", "jmp")], &lines[i + 1..]].concat())
             }
-        } else if lines[i].starts_with("nop") {
-            let new_line = lines[i].replace("nop", "jmp");
-            match part1(&[&lines[0..i], &[&new_line], &lines[i + 1..]].concat()) {
-                Err(_) => (),
-                r => return r,
-            }
-        }
-    }
-    Err(RunError::NoResult)
+        })
+        .filter(|r| r.is_ok())
+        .next()
+        .unwrap_or(Err(RunError::NoResult))
 }
 
 #[cfg(test)]
