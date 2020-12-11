e93f58face91a6b7005bae2a740f163cb17b03e1
Jacob Pedersen <jacobsqy@teknik.io>
2020-12-11 16:08:01 +0100

    Day11: Shorten the code and add benchmarks

--- src/day11.rs
+++ src/day11.rs
@@ -11,115 +11,71 @@ pub fn run(puzzle: &Puzzle) -> Result<RunResult, RunError> {
 }
 
 fn parse(input: &str) -> Vec<Vec<char>> {
-    let mut result: Vec<Vec<char>> = Vec::new();
-
-    for line in input.lines() {
-        result.push(line.chars().collect())
-    }
-    result
+    input.lines().map(|x| x.chars().collect()).collect()
 }
 
 fn part1(input: &str) -> u32 {
     let mut seats: Vec<Vec<char>> = parse(input);
-    let mut changed = true;
-    while changed {
-        changed = false;
-        let mut new_seats: Vec<Vec<char>> = seats.clone();
+    let mut new_seats: Vec<(usize, usize, char)> = vec![(0, 0, '0')];
+    while new_seats.len() > 0 {
+        new_seats.clear();
 
         for i in 0..seats.len() {
             for (j, seat) in seats[i].iter().enumerate() {
                 if *seat == 'L' && occupied_adjacent_seats(i as i32, j as i32, &seats) == 0 {
-                    new_seats[i][j] = '#';
-                    changed = true;
+                    new_seats.push((i, j, '#'));
                 } else if *seat == '#' && occupied_adjacent_seats(i as i32, j as i32, &seats) > 3 {
-                    new_seats[i][j] = 'L';
-                    changed = true;
+                    new_seats.push((i, j, 'L'));
                 }
             }
         }
-        seats = new_seats;
-    }
 
-    let mut occupied_seats = 0;
-    for i in 0..seats.len() {
-        for j in 0..seats[i].len() {
-            if seats[i][j] == '#' {
-                occupied_seats += 1;
-            }
+        for (x, y, v) in &new_seats {
+            seats[*x][*y] = *v;
         }
     }
-    occupied_seats
+
+    seats.concat().iter().filter(|c| **c == '#').count() as u32
 }
 
 fn part2(input: &str) -> u32 {
     let mut seats: Vec<Vec<char>> = parse(input);
-    let mut changed = true;
-    while changed {
-        changed = false;
-        let mut new_seats: Vec<Vec<char>> = seats.clone();
+    let mut new_seats: Vec<(usize, usize, char)> = vec![(0, 0, '0')];
+    while new_seats.len() > 0 {
+        new_seats.clear();
 
         for i in 0..seats.len() {
             for (j, seat) in seats[i].iter().enumerate() {
                 if *seat == 'L' && occupied_visible_seats(i as i32, j as i32, &seats) == 0 {
-                    new_seats[i][j] = '#';
-                    changed = true;
+                    new_seats.push((i, j, '#'));
                 } else if *seat == '#' && occupied_visible_seats(i as i32, j as i32, &seats) > 4 {
-                    new_seats[i][j] = 'L';
-                    changed = true;
+                    new_seats.push((i, j, 'L'));
                 }
             }
         }
-        seats = new_seats;
-    }
 
-    let mut occupied_seats = 0;
-    for i in 0..seats.len() {
-        for j in 0..seats[i].len() {
-            if seats[i][j] == '#' {
-                occupied_seats += 1;
-            }
+        for (x, y, v) in &new_seats {
+            seats[*x][*y] = *v;
         }
     }
-    occupied_seats
+
+    seats.concat().iter().filter(|c| **c == '#').count() as u32
 }
 
 fn occupied_visible_seats(x: i32, y: i32, seats: &Vec<Vec<char>>) -> u32 {
     let mut result = 0;
-    let plus = &|a, i| a + i;
-    let minus = &|a, i| a - i;
-    let same = &|a, _i| a;
-    // North-west
-    if seat_is_visible(x, y, minus, minus, seats) {
-        result += 1;
+    for i in -1..=1 {
+        let change_x = &|a, index| a + i * index;
+        for j in -1..=1 {
+            let change_y = &|a, index| a + j * index;
+            if !(i == 0 && j == 0) {
+                if seat_is_visible(x, y, change_x, change_y, seats) {
+                    result += 1;
+                }
+            }
+        }
     }
-    // North
-    if seat_is_visible(x, y, same, minus, seats) {
-        result += 1;
-    }
-    // North-east
-    if seat_is_visible(x, y, plus, minus, seats) {
-        result += 1;
-    }
-    // East
-    if seat_is_visible(x, y, minus, same, seats) {
-        result += 1;
-    }
-    // West
-    if seat_is_visible(x, y, plus, same, seats) {
-        result += 1;
-    }
-    // South-west
-    if seat_is_visible(x, y, minus, plus, seats) {
-        result += 1;
-    }
-    // South
-    if seat_is_visible(x, y, same, plus, seats) {
-        result += 1;
-    }
-    // South-east
-    if seat_is_visible(x, y, plus, plus, seats) {
-        result += 1;
-    }
+
     result
 }
 
@@ -145,29 +101,14 @@ fn seat_is_visible(
 
 fn occupied_adjacent_seats(x: i32, y: i32, seats: &Vec<Vec<char>>) -> u32 {
     let mut result = 0;
-    if seat_is_occupied(x - 1, y - 1, &seats) == Some('#') {
-        result += 1;
-    }
-    if seat_is_occupied(x, y - 1, &seats) == Some('#') {
-        result += 1;
-    }
-    if seat_is_occupied(x + 1, y - 1, &seats) == Some('#') {
-        result += 1;
-    }
-    if seat_is_occupied(x - 1, y, &seats) == Some('#') {
-        result += 1;
-    }
-    if seat_is_occupied(x + 1, y, &seats) == Some('#') {
-        result += 1;
-    }
-    if seat_is_occupied(x - 1, y + 1, &seats) == Some('#') {
-        result += 1;
-    }
-    if seat_is_occupied(x, y + 1, &seats) == Some('#') {
-        result += 1;
-    }
-    if seat_is_occupied(x + 1, y + 1, &seats) == Some('#') {
-        result += 1;
+    for i in -1..=1 {
+        for j in -1..=1 {
+            if !(i == 0 && j == 0) {
+                if seat_is_occupied(x + i, y + j, &seats) == Some('#') {
+                    result += 1;
+                }
+            }
+        }
     }
     result
 }
@@ -187,4 +128,16 @@ fn seat_is_occupied(x: i32, y: i32, seats: &Vec<Vec<char>>) -> Option<char> {
 mod tests {
     use super::*;
     use test::Bencher;
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let puzzle: Puzzle = Puzzle::new("11", "1", None).unwrap();
+        b.iter(|| part1(&puzzle.input));
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let puzzle: Puzzle = Puzzle::new("11", "2", None).unwrap();
+        b.iter(|| part2(&puzzle.input));
+    }
 }
