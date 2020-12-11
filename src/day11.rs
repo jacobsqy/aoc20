2b0872e0c713ca0801ef8073cfaf9dbd6c2d036f
Jacob Pedersen <jacobsqy@teknik.io>
2020-12-11 12:40:36 +0100

    Day11: Complete

--- src/day11.rs
+++ src/day11.rs
@@ -6,8 +6,7 @@ use crate::RunResult;
 pub fn run(puzzle: &Puzzle) -> Result<RunResult, RunError> {
     match puzzle.part {
         Part::One => Ok(RunResult::U32(part1(&puzzle.input))),
-        //Part::Two => Ok(RunResult::U32(part2(&puzzle.input))),
-        _ => Err(RunError::NoResult),
+        Part::Two => Ok(RunResult::U32(part2(&puzzle.input))),
     }
 }
 
@@ -52,40 +51,136 @@ fn part1(input: &str) -> u32 {
     occupied_seats
 }
 
-fn occupied_adjacent_seats(x: i32, y: i32, seats: &Vec<Vec<char>>) -> u32 {
+fn part2(input: &str) -> u32 {
+    let mut seats: Vec<Vec<char>> = parse(input);
+    let mut changed = true;
+    while changed {
+        changed = false;
+        let mut new_seats: Vec<Vec<char>> = seats.clone();
+
+        for i in 0..seats.len() {
+            for (j, seat) in seats[i].iter().enumerate() {
+                if *seat == 'L' && occupied_visible_seats(i as i32, j as i32, &seats) == 0 {
+                    new_seats[i][j] = '#';
+                    changed = true;
+                } else if *seat == '#' && occupied_visible_seats(i as i32, j as i32, &seats) > 4 {
+                    new_seats[i][j] = 'L';
+                    changed = true;
+                }
+            }
+        }
+        seats = new_seats;
+    }
+
+    let mut occupied_seats = 0;
+    for i in 0..seats.len() {
+        for j in 0..seats[i].len() {
+            if seats[i][j] == '#' {
+                occupied_seats += 1;
+            }
+        }
+    }
+    occupied_seats
+}
+
+fn occupied_visible_seats(x: i32, y: i32, seats: &Vec<Vec<char>>) -> u32 {
     let mut result = 0;
-    if seat_is_occupied(x - 1, y - 1, &seats).unwrap_or(false) {
+    let plus = &|a, i| a + i;
+    let minus = &|a, i| a - i;
+    let same = &|a, _i| a;
+    // North-west
+    if seat_is_visible(x, y, minus, minus, seats) {
         result += 1;
     }
-    if seat_is_occupied(x, y - 1, &seats).unwrap_or(false) {
+    // North
+    if seat_is_visible(x, y, same, minus, seats) {
         result += 1;
     }
-    if seat_is_occupied(x + 1, y - 1, &seats).unwrap_or(false) {
+    // North-east
+    if seat_is_visible(x, y, plus, minus, seats) {
         result += 1;
     }
-    if seat_is_occupied(x - 1, y, &seats).unwrap_or(false) {
+    // East
+    if seat_is_visible(x, y, minus, same, seats) {
         result += 1;
     }
-    if seat_is_occupied(x + 1, y, &seats).unwrap_or(false) {
+    // West
+    if seat_is_visible(x, y, plus, same, seats) {
         result += 1;
     }
-    if seat_is_occupied(x - 1, y + 1, &seats).unwrap_or(false) {
+    // South-west
+    if seat_is_visible(x, y, minus, plus, seats) {
         result += 1;
     }
-    if seat_is_occupied(x, y + 1, &seats).unwrap_or(false) {
+    // South
+    if seat_is_visible(x, y, same, plus, seats) {
         result += 1;
     }
-    if seat_is_occupied(x + 1, y + 1, &seats).unwrap_or(false) {
+    // South-east
+    if seat_is_visible(x, y, plus, plus, seats) {
         result += 1;
     }
     result
 }
 
-fn seat_is_occupied(x: i32, y: i32, seats: &Vec<Vec<char>>) -> Option<bool> {
-    if x >= 0 && y >= 0 && *seats.get(x as usize)?.get(y as usize)? == '#' {
-        return Some(true);
+fn seat_is_visible(
+    x: i32,
+    y: i32,
+    change_x: &dyn Fn(i32, i32) -> i32,
+    change_y: &dyn Fn(i32, i32) -> i32,
+    seats: &Vec<Vec<char>>,
+) -> bool {
+    let mut i = 1;
+    loop {
+        match seat_is_occupied(change_x(x, i), change_y(y, i), seats) {
+            // The seat is occupied
+            Some('#') => return true,
+            // The seat is floor
+            Some('.') => i += 1,
+            // The border has been reached or the seat is empty
+            _ => return false,
+        }
     }
-    Some(false)
+}
+
+fn occupied_adjacent_seats(x: i32, y: i32, seats: &Vec<Vec<char>>) -> u32 {
+    let mut result = 0;
+    if seat_is_occupied(x - 1, y - 1, &seats) == Some('#') {
+        result += 1;
+    }
+    if seat_is_occupied(x, y - 1, &seats) == Some('#') {
+        result += 1;
+    }
+    if seat_is_occupied(x + 1, y - 1, &seats) == Some('#') {
+        result += 1;
+    }
+    if seat_is_occupied(x - 1, y, &seats) == Some('#') {
+        result += 1;
+    }
+    if seat_is_occupied(x + 1, y, &seats) == Some('#') {
+        result += 1;
+    }
+    if seat_is_occupied(x - 1, y + 1, &seats) == Some('#') {
+        result += 1;
+    }
+    if seat_is_occupied(x, y + 1, &seats) == Some('#') {
+        result += 1;
+    }
+    if seat_is_occupied(x + 1, y + 1, &seats) == Some('#') {
+        result += 1;
+    }
+    result
+}
+
+fn seat_is_occupied(x: i32, y: i32, seats: &Vec<Vec<char>>) -> Option<char> {
+    if x >= 0 && y >= 0 {
+        match *seats.get(x as usize)?.get(y as usize)? {
+            '#' => return Some('#'),
+            'L' => return Some('L'),
+            _ => return Some('.'),
+        }
+    }
+    None
 }
 
 #[cfg(test)]
