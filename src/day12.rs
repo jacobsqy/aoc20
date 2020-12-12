37d44b78e9ee182248d4fd92da57969ea16c5420
Jacob Pedersen <jacobsqy@teknik.io>
2020-12-12 17:59:36 +0100

    Day12: Refine part 2

--- src/day12.rs
+++ src/day12.rs
@@ -11,116 +11,52 @@ pub fn run(puzzle: &Puzzle) -> Result<RunResult, RunError> {
     }
 }
 
-struct Ship {
-    ew_pos: i32,
-    ns_pos: i32,
-    waypoint: Waypoint,
+fn forward_waypoint(waypoint: &(i32, i32), ship: &(i32, i32), x: i32) -> (i32, i32) {
+    (ship.0 + (waypoint.0 * x), ship.1 + (waypoint.1 * x))
 }
 
-impl Ship {
-    fn forward(&mut self, x: i32) {
-        self.ew_pos += self.waypoint.ew_pos * x;
-        self.ns_pos += self.waypoint.ns_pos * x;
+fn right_waypoint(waypoint: &(i32, i32), x: i32) -> (i32, i32) {
+    let (mut ns, mut ew) = *waypoint;
+    for _ in 0..x / 90 {
+        let new_ew_pos = ns;
+        let new_ns_pos = -ew;
+        ns = new_ns_pos;
+        ew = new_ew_pos;
     }
+    (ns, ew)
 }
 
-struct Waypoint {
-    ew_pos: i32,
-    ns_pos: i32,
-}
-
-impl Waypoint {
-    fn right(&mut self, x: i32) {
-        for _ in 0..x / 90 {
-            self.right_90();
-        }
+fn left_waypoint(waypoint: &(i32, i32), x: i32) -> (i32, i32) {
+    let (mut ns, mut ew) = *waypoint;
+    for _ in 0..x / 90 {
+        let new_ew_pos = -ns;
+        let new_ns_pos = ew;
+        ns = new_ns_pos;
+        ew = new_ew_pos;
     }
-
-    fn left(&mut self, x: i32) {
-        for _ in 0..x / 90 {
-            self.left_90();
-        }
-    }
-
-    fn left_90(&mut self) {
-        let new_ew_pos;
-        let new_ns_pos;
-        if self.ew_pos > 0 && self.ns_pos > 0 {
-            // First quadrant
-            new_ew_pos = -self.ns_pos;
-            new_ns_pos = self.ew_pos;
-        } else if self.ew_pos > 0 {
-            // Second quadrant
-            new_ew_pos = -self.ns_pos;
-            new_ns_pos = self.ew_pos;
-        } else if self.ns_pos < 0 {
-            // Third quadrant
-            new_ew_pos = -self.ns_pos;
-            new_ns_pos = self.ew_pos;
-        } else {
-            // Fourth quadrant
-            new_ew_pos = -self.ns_pos;
-            new_ns_pos = self.ew_pos;
-        }
-        self.ns_pos = new_ns_pos;
-        self.ew_pos = new_ew_pos;
-    }
-
-    fn right_90(&mut self) {
-        let new_ew_pos;
-        let new_ns_pos;
-        if self.ew_pos > 0 && self.ns_pos > 0 {
-            // First quadrant
-            new_ew_pos = self.ns_pos;
-            new_ns_pos = -self.ew_pos;
-        } else if self.ew_pos > 0 {
-            // Second quadrant
-            new_ew_pos = self.ns_pos;
-            new_ns_pos = -self.ew_pos;
-        } else if self.ns_pos < 0 {
-            // Third quadrant
-            new_ew_pos = self.ns_pos;
-            new_ns_pos = -self.ew_pos;
-        } else {
-            // Fourth quadrant
-            new_ew_pos = self.ns_pos;
-            new_ns_pos = -self.ew_pos;
-        }
-        self.ns_pos = new_ns_pos;
-        self.ew_pos = new_ew_pos;
-    }
+    (ns, ew)
 }
 
 fn part2(input: &str) -> i32 {
-    let mut ship = Ship {
-        ew_pos: 0,
-        ns_pos: 0,
-        waypoint: Waypoint {
-            ew_pos: 10,
-            ns_pos: 1,
-        },
-    };
+    // (ns, ew)
+    let mut ship = (0, 0);
+    // (ns, ew)
+    let mut waypoint = (1, 10);
 
     for line in input.lines() {
         match line.split_at(1) {
-            ("N", x) => ship.waypoint.ns_pos += x.parse::<i32>().unwrap(),
-            ("S", x) => ship.waypoint.ns_pos -= x.parse::<i32>().unwrap(),
-            ("E", x) => ship.waypoint.ew_pos += x.parse::<i32>().unwrap(),
-            ("W", x) => ship.waypoint.ew_pos -= x.parse::<i32>().unwrap(),
-            ("L", x) => ship.waypoint.left(x.parse().unwrap()),
-            ("R", x) => ship.waypoint.right(x.parse().unwrap()),
-            ("F", x) => ship.forward(x.parse().unwrap()),
+            ("N", x) => waypoint.0 += x.parse::<i32>().unwrap(),
+            ("S", x) => waypoint.0 -= x.parse::<i32>().unwrap(),
+            ("E", x) => waypoint.1 += x.parse::<i32>().unwrap(),
+            ("W", x) => waypoint.1 -= x.parse::<i32>().unwrap(),
+            ("L", x) => waypoint = left_waypoint(&waypoint, x.parse().unwrap()),
+            ("R", x) => waypoint = right_waypoint(&waypoint, x.parse().unwrap()),
+            ("F", x) => ship = forward_waypoint(&waypoint, &ship, x.parse().unwrap()),
             _ => panic!("Could not parse line! {}", line),
         }
-        println!("\n{}", line);
-        println!("Ship coordinates: NS: {}, EW: {}", ship.ns_pos, ship.ew_pos);
-        println!(
-            "Waypoint coordinates: NS: {}, EW: {}",
-            ship.waypoint.ns_pos, ship.waypoint.ew_pos
-        );
     }
 
-    ship.ns_pos.abs() + ship.ew_pos.abs()
+    ship.0.abs() + ship.1.abs()
 }
 
 #[cfg(test)]
