e19d7bdeb0b000ae0777fb42adfc5536c58f447b
Jacob Pedersen <jacobsqy@teknik.io>
2020-12-15 14:55:35 +0100

    Day15: Make it a bit funcy

--- src/day15.rs
+++ src/day15.rs
@@ -22,35 +22,35 @@ fn part2(input: &str) -> u32 {
 fn solve(input: &str, n: u32) -> u32 {
     let mut values: HashMap<u32, u32> = HashMap::new();
     let mut i = 1;
-    let mut prev_value = None;
-    for line in input
+    let input = input
         .lines()
         .next()
         .unwrap()
         .split(',')
-        .map(|l| l.parse::<u32>().unwrap())
-    {
-        prev_value = values.insert(line, i).map(|old_i| i - old_i);
+        .map(|l| l.parse::<u32>().unwrap());
+
+    for line in input {
+        values.insert(line, i);
         i += 1;
     }
 
-    for j in i..n {
-        match prev_value {
-            Some(x) => {
-                prev_value = values.insert(x, j).map(|old_j| j - old_j);
-            }
-            None => {
-                prev_value = values.insert(0, j).map(|old_j| j - old_j);
-            }
-        }
-    }
-    prev_value.unwrap()
+    (i..n)
+        .fold(None, |value, j| {
+            values.insert(value.unwrap_or(0), j).map(|old_j| j - old_j)
+        })
+        .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use test::Bencher;
+
+    #[test]
+    fn test_part2() {
+        let puzzle: Puzzle = Puzzle::new("15", "2", None).unwrap();
+        assert_eq!(part2(&puzzle.input), 37385);
+    }
 
     #[bench]
     fn bench_part1(b: &mut Bencher) {
